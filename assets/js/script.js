05d4afa6de6c1ce712f6f061a7441dc7c140c1ca
cek333 <69444825+cek333@users.noreply.github.com>
2020-09-19 14:17:36 -0400

    added character type prompts and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  // Get password length from user
   var pswdLenStr;
   var pswdLen = 0;
   while(Number.isNaN(pswdLen) || pswdLen<8 || pswdLen>128) {
@@ -11,7 +12,23 @@ function generatePassword() {
       alert("Please enter a number from 8 to 128");
     }
   }
-  console.log("pswd length:" + pswdLen);
+  //console.log("pswd length:" + pswdLen);
+
+  // Get character types from user
+  var includeLowercase = false;
+  var includeUppercase = false;
+  var includeNumbers = false;
+  var includeSpecial = false;
+  while(!(includeLowercase || includeUppercase || includeNumbers || includeSpecial)) {
+    includeLowercase = confirm("Should the password include lowercase letters? [OK/Yes][Cancel/No]");
+    includeUppercase = confirm("Should the password include uppercase letters? [OK/Yes][Cancel/No]");
+    includeNumbers = confirm("Should the password include numbers? [OK/Yes][Cancel/No]");
+    includeSpecial = confirm("Should the password include special characters? [OK/Yes][Cancel/No]");
+    if (!(includeLowercase || includeUppercase || includeNumbers || includeSpecial)) {
+      alert("The password must include at least one of the following types: lowercase, uppercase, numbers, or speical characters.");
+    }
+  }
+  console.log(`lowercase:${includeLowercase}, uppercase:${includeUppercase}, numbers:${includeNumbers}, special:${includeSpecial}`);
   return "Password";
 } 
 
