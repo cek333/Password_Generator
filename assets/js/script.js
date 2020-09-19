d5835ddd9aacc84dbefd48c75474a5dec12d79d5
cek333 <69444825+cek333@users.noreply.github.com>
2020-09-19 14:01:19 -0400

    added password length prompt and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  var pswdLenStr;
+  var pswdLen = 0;
+  while(Number.isNaN(pswdLen) || pswdLen<8 || pswdLen>128) {
+    pswdLenStr = prompt("Please enter password length [in the range 8-128]");
+    pswdLen = Number(pswdLenStr);
+    if (Number.isNaN(pswdLen) || pswdLen<8 || pswdLen>128) {
+      alert("Please enter a number from 8 to 128");
+    }
+  }
+  console.log("pswd length:" + pswdLen);
   return "Password";
 } 
 
